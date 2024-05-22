b167dbbb62ea6f58d81f89a62272f29943c2e0ce
deadlypantsd <44475630+deadlypantsd@users.noreply.github.com>
2024-05-22 12:59:13 -0400

    Update nowplaying.sh
    typo

--- nowplaying.sh
+++ nowplaying.sh
@@ -1,6 +1,6 @@
 while true;
 do
   export NOWPLAYING=`tail -n 2 ~/nowplaying.txt | head -n 1 | sed 's/.*|>\ \ //g' | sed 's/\ on.*//g' | awk -F '\ on\ ' '{print $2 $1 ;}' | sed 's:\ by\ :\ -\ :g'|sed 's/\"//g' | urlencode`;
-  curl -s --user $sHOUT_AUTH -X "GET" "http://$SHOUT_SERVER/admin/metadata.xsl?song=$NOWPLAYING&mount=%2F$SHOUT_MOUNT&mode=updinfo" > /dev/null;
+  curl -s --user $SHOUT_AUTH -X "GET" "http://$SHOUT_SERVER/admin/metadata.xsl?song=$NOWPLAYING&mount=%2F$SHOUT_MOUNT&mode=updinfo" > /dev/null;
   sleep 2;
 done
